b2cf420e7fa3d14f2cfa612debb5b6af0f13cc10
yannformation <cotardyann@orange.fr>
2022-11-30 14:42:16 +0100

    2eme partie exercice SERVICE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Task } from './class/task.model';
-import { TodoListService } from './services/todo-list.service';
+import { TodolistService } from './services/todo-list.service';
 
 @Component({
   selector: 'app-root',
@@ -12,19 +12,19 @@ export class AppComponent {
   public tasks!: Task[];
   public percentage!: number;
 
-  constructor(public todo: TodoListService) {
+  constructor(public todoList: TodolistService) {
     new Promise(() =>
       setTimeout(() => {
 
       }, 3000)
     )
-    this.count = this.tasks.filter((task) => task.completed).length;
+    this.count = this.todoList.tasks.filter((task) => task.completed).length;
 
-    this.percentage = (this.count / this.tasks.length) * 100;
+    this.percentage = (this.count / this.todoList.tasks.length) * 100;
   };
   changeCount(status: boolean): void {
     this.count = status ? this.count + 1 : this.count - 1;
-    this.percentage = (this.count / this.tasks.length) * 100; //pour mettre àjour le pourcentage à chaque changement de status de "count"
+    this.percentage = (this.count / this.todoList.tasks.length) * 100; //pour mettre àjour le pourcentage à chaque changement de status de "count"
   }
 
   trackByFunction(index: number, item: any): string {
--- src/app/services/todo-list.service.ts
+++ src/app/services/todo-list.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Task } from '../class/task.model';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class TodoListService {
-  public tasks: Task[];
-
-  constructor() {
-    this.tasks = [
+  const updateList: Task[] = [
       new Task(1, 'Acheter du pain', false, 'chez le boulanger', Date()),
       new Task(2, "acheter de l'eau", true, "chez l'épicier", Date()),
       new Task(3, 'acheter du beurre', false, 'à la crèmerie', Date()),
       new Task(4, 'Faire le ménage', true, 'chambre et salon', Date()),
     ];
 
-   }
-}
+    @Injectable({
+      providedIn: 'root'
+    })
+    export class TodolistService {
+      public tasks: Task[];
+
+      constructor() {
+        this.tasks = updateList;
+      }
+    }
