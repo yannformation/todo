90c8b653b1fe017147d09f05d820002dd6d3ae3a
yannformation <cotardyann@orange.fr>
2022-11-30 10:26:23 +0100

    ngClass style count

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,10 +15,10 @@ export class AppComponent {
     new Promise(() =>
       setTimeout(() => {
         this.tasks = [
-          new Task(1, 'Acheter du pain', true, 'chez le boulanger', Date()),
+          new Task(1, 'Acheter du pain', false, 'chez le boulanger', Date()),
           new Task(2, "acheter de l'eau", true, "chez l'épicier", Date()),
           new Task(3, 'acheter du beurre', false, 'à la crèmerie', Date()),
-          new Task(4, 'Faire le ménage', false, 'chambre et salon', Date()),
+          new Task(4, 'Faire le ménage', true, 'chambre et salon', Date()),
         ];
         this.count = this.tasks.filter((task) => task.completed).length;
 
@@ -28,7 +28,7 @@ export class AppComponent {
   };
   changeCount(status: boolean): void {
     this.count = status ? this.count + 1 : this.count - 1;
-    this.percentage = (this.count / this.tasks.length) * 100; //pour mettre àjour le pourcentage
+    this.percentage = (this.count / this.tasks.length) * 100; //pour mettre àjour le pourcentage à chaque changement de status de "count"
   }
 
   trackByFunction(index: number, item: any): string {
