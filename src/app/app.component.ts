ea1fa2f32b51baef5c8d775c43b9fc6fe3879881
yannformation <cotardyann@orange.fr>
2022-12-01 10:21:07 +0100

    2eme exercice updateList réussi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,26 +8,29 @@ import { TodolistService } from './services/todo-list.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public count!: number;
-  public tasks!: Task[];
-  public percentage!: number;
+  // public count!: number;
+  // public percentage!: number;
+
 
   constructor(public todoList: TodolistService) {
-    new Promise(() =>
-      setTimeout(() => {
-
-      }, 3000)
-    )
-    this.count = this.todoList.tasks.filter((task) => task.completed).length;
-
-    this.percentage = (this.count / this.todoList.tasks.length) * 100;
+    // this.count = this.todoList.tasks?.filter((task) => task.completed).length;
+    // this.percentage = (this.count / this.todoList.tasks?.length) * 100;
   };
-  changeCount(status: boolean): void {
-    this.count = status ? this.count + 1 : this.count - 1;
-    this.percentage = (this.count / this.todoList.tasks.length) * 100; //pour mettre àjour le pourcentage à chaque changement de status de "count"
+  public get count(): number {
+    return (this.todoList.tasks?.length > 0) ? this.todoList.tasks?.filter(task => task.completed).length : 0;
   }
+
+  public get percentage(): number {
+    return (this.todoList.tasks?.length > 0) ? (this.count / this.todoList.tasks?.length)*100 : 0;
+  }
+
+  // changeCount(status: boolean): void {
+  //   this.count = status ? this.count + 1 : this.count - 1;
+  //   this.percentage = (this.count / this.todoList.tasks?.length) * 100; //pour mettre àjour le pourcentage à chaque changement de status de "count"
+  // }
 
   trackByFunction(index: number, item: any): string {
     return item.id;
   }
+
 }
--- src/app/services/todo-list.service.ts
+++ src/app/services/todo-list.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Task } from '../class/task.model';
 
-  const updateList: Task[] = [
-      new Task(1, 'Acheter du pain', false, 'chez le boulanger', Date()),
-      new Task(2, "acheter de l'eau", true, "chez l'épicier", Date()),
-      new Task(3, 'acheter du beurre', false, 'à la crèmerie', Date()),
-      new Task(4, 'Faire le ménage', true, 'chambre et salon', Date()),
-    ];
+const initialList: Task[] = [
+  new Task(1, 'Acheter du pain', false, 'chez le boulanger', Date()),
+  new Task(2, "acheter de l'eau", true, "chez l'épicier", Date()),
+  new Task(3, 'acheter du beurre', false, 'à la crèmerie', Date()),
+  new Task(4, 'Faire le ménage', true, 'chambre et salon', Date()),
+];
 
-    @Injectable({
-      providedIn: 'root'
-    })
-    export class TodolistService {
-      public tasks: Task[];
+@Injectable({
+  providedIn: 'root',
+})
+export class TodolistService {
+  public tasks!: Task[];
 
-      constructor() {
-        this.tasks = updateList;
-      }
-    }
+  constructor() {
+   this.updateList(initialList);
+  }
+  updateList(list: Task[]): any {
+    new Promise<Task[]>(() => {
+      setTimeout(() => {
+
+        this.tasks = list;
+      }, 3000);
+    });
+  }
+}
